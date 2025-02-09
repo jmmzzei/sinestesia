8c4ba4cc70b6e0bbeccbde55b47d3eee2093584f
José María <josemmazzei@gmail.com>
2025-02-09 15:29:03 -0300

    Adjust deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust the base if using subdirectories
+  base: 'https://jmmzzei.github.io/sinestesia', // Adjust the base if using subdirectories
   optimizeDeps: {
     include: ["pdfjs-dist"], // Ensure it's included in dependency pre-bundling
   },
